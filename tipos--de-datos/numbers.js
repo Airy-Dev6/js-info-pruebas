ca9c0874f52e33a1268817f8a1b70d27c23f2925
Airy-Dev6 <airysilva11@gmail.com>
2025-01-21 19:50:13 -0600

    new orden add tdd

---
+++ tipos--de-datos/numbers.js
@@ -0,0 +1,27 @@
+// Números
+
+// EJERCICIOS
+
+// Suma números del visitante
+
+// Crea un script que pida al visitante que ingrese dos números y muestre la suma
+
+// let a = +prompt(`Ingresa el primer valor: `);
+
+// let b = +prompt("Ingresa el segundo valor: ");
+
+// alert(`La suma de ${a} + ${b} es : ${a + b}`);
+
+// --------------------------------------
+
+// ¿Por qué 6.35.toFixed(1)== 6.3?
+
+// let n = 6.35;
+
+// console.log(n.toFixed(1));
+
+// console.log(Math.round(n * 10) / 10);
+
+// console.log(+(n + 0.001).toFixed(1));
+
+// console.log(+(6.35).toPrecision(2));
